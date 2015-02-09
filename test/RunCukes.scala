48f9b3af3347f9b23bed0876c4d5f7b22689c53a
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-02-09 21:38:36 +0000

    Configure Cucumber test runner to ignore @pending tag.

--- test/RunCukes.scala
+++ test/RunCukes.scala
@@ -3,5 +3,7 @@ import cucumber.api.junit.Cucumber
 import org.junit.runner.RunWith
 
 @RunWith(classOf[Cucumber])
-@CucumberOptions(glue = Array("steps"))
+@CucumberOptions(
+  glue = Array("steps"),
+  tags = Array("~@pending"))
 class RunCukes
