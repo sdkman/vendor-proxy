e69053fa60646f27b9b29ec1858a7d773d10a33c
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-02-09 21:37:24 +0000

    Makes create vendor request chain reactive.

--- app/controllers/Vendors.scala
+++ app/controllers/Vendors.scala
@@ -1,18 +1,23 @@
 package controllers
 
-import domain.VendorPersistence
+import domain.{VendorPersistence, VendorPersistenceException}
 import play.api.libs.json.Json.toJson
 import play.api.mvc._
 import play.modules.reactivemongo.MongoController
 import security.Authorised
 import utils.{ErrorMarshalling, VendorMarshalling}
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 object Vendors extends Controller with MongoController with VendorPersistence with VendorMarshalling with ErrorMarshalling {
 
   def create = Authorised(parse.json) { req =>
-    req.body.validate[Request].asOpt.fold(BadRequest(badRequestMsg)) { jsonReq =>
-      val vendor = persist(jsonReq.vendor)
-      Created(toJson(Response(vendor._id, vendor.token, vendor.name)))
+    req.body.validate[Request].asOpt.fold(Future(BadRequest(badRequestMsg))) { vendorReq =>
+      for {
+        vendorO <- persist(vendorReq.vendor)
+        vendor = vendorO.getOrElse(throw VendorPersistenceException("persistence issue"))
+      } yield Created(toJson(Response(vendor._id, vendor.token, vendor.name)))
     }
   }
 
--- app/domain/Vendor.scala
+++ app/domain/Vendor.scala
@@ -5,6 +5,8 @@ import play.api.libs.json.Json
 import play.modules.reactivemongo.json.collection.JSONCollection
 import utils.TokenGenerator
 
+import scala.concurrent.Future
+
 case class Vendor(_id: String, name: String, token: String)
 
 object Vendor {
@@ -16,14 +18,16 @@ trait VendorPersistence {
   import domain.Vendor._
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  def collection: JSONCollection = db.collection[JSONCollection]("vendors")
+  lazy val collection: JSONCollection = db.collection[JSONCollection]("vendors")
 
-  def persist(name: String): Vendor = {
+  def persist(name: String): Future[Option[Vendor]] = {
     val oid = TokenGenerator.generateConsumerKey(name)
     val token = TokenGenerator.generateSHAToken(name)
     val vendor = Vendor(oid, name, token)
-    collection.insert(vendor)
-    vendor
+    for {
+      lastError <- collection.insert(vendor)
+      vendorO = if (!lastError.inError) Some(vendor) else None
+    } yield vendorO
   }
 
   def succMsg(vendor: String) = s"Persisted $vendor"
@@ -31,3 +35,5 @@ trait VendorPersistence {
   def errMsg(vendor: String) = s"Can not persist $vendor"
 
 }
+
+case class VendorPersistenceException(m: String) extends RuntimeException(m: String)
