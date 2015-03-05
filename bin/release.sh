32be55d8cdfe6d9b63c9e7dbfb2915ea56ffa9e1
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-03-05 09:10:39 +0000

    Adds build and release shell scripts for CI.

---
+++ bin/release.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+VERSION="$1"
+BRANCH="RELEASE"
+
+#sanity
+if [[ -z "$VERSION" ]]; then
+	echo "Usage: release.sh <version>"
+	exit 0
+fi
+
+#prepare branch
+git checkout master
+git branch -D "$BRANCH"
+git checkout -b "$BRANCH"
+
+#update version
+sed -i "s/1.0.0-SNAPSHOT/$VERSION/g" gradle.properties
+git add gradle.properties
+git commit -m "Update version of $BRANCH to $VERSION"
+
+#push branch
+git push -f origin "$BRANCH:$BRANCH"
+
+#push tag
+git tag "$VERSION"
+git push origin "$VERSION"
+
+#back to master branch
+git checkout master
