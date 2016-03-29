c5fa85dcc9ee17ea3fc6a64f8f1da56cecbaa89e
Marco Vermeulen <vermeulen.mp@gmail.com>
2016-03-29 23:14:58 +0100

    Add useful logging on Consumer persistence.

--- app/controllers/Consumers.scala
+++ app/controllers/Consumers.scala
@@ -1,13 +1,14 @@
 package controllers
 
 import domain.{Consumer, ConsumerPersistence}
+import play.api.Logger._
 import play.api.libs.json.Json.toJson
 import play.api.mvc._
 import play.modules.reactivemongo.MongoController
 import reactivemongo.core.commands.LastError
 import security.AsAdministrator
 import utils.TokenGenerator.sha256
-import utils.{ErrorMarshalling, ConsumerMarshalling}
+import utils.{ConsumerMarshalling, ErrorMarshalling}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -19,10 +20,17 @@ object Consumers extends Controller with MongoController with ConsumerPersistenc
     req.body.validate[Request].asOpt.fold(Future(BadRequest(badRequestMsg))) { consumerReq =>
       val consumer = Consumer.fromName(consumerReq.consumer)
       persist(consumer.copy(token = sha256(consumer.token))).map {
-        case le if le.ok => Created(toJson(Response(consumer._id, consumer.token, consumer.name)))
+        case le if le.ok =>
+          info(s"Successfully persisted Consumer: ${consumer.name}")
+          Created(toJson(Response(consumer._id, consumer.token, consumer.name)))
       }.recover {
-        case LastError(_, _, Some(11000), _, _, _, _) => Conflict(conflictMsg(consumer.name))
-        case _ => InternalServerError(internalServerErrorMsg)
+        case LastError(_, _, Some(11000), _, _, _, _) =>
+          error(s"Conflict on persisting Consumer: ${consumer.name}")
+          Conflict(conflictMsg(consumer.name))
+        case LastError(ok, err, code, errMsg, doc, n, updatedExisting) =>
+          val message = s"Error on persisting Consumer: ${consumer.name} - code:${code.getOrElse("no code")} err:${err.getOrElse("no message")}"
+          error(message)
+          InternalServerError(message)
       }
     }
   }
