c8cb0b6651a7858839108da611d9b552bd6ea8a6
Marco Vermeulen <vermeulen.mp@gmail.com>
2016-03-30 09:42:47 +0100

    Rename default database to sdkman.

--- app/controllers/Proxy.scala
+++ app/controllers/Proxy.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import play.api.Logger
 import play.api.Play._
 import play.api.libs.ws.WS
 import play.api.mvc.Controller
@@ -12,6 +13,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object Proxy extends Controller with RequestHeaders {
 
   def execute(service: String) = AsConsumer(parse.json) { (request, consumerName) =>
+    Logger.info(s"Proxying $service on behalf of $consumerName")
     WS.url(apiUrl(service))
       .withHeaders(tokenHeader(service), consumerHeader(consumerName))
       .withMethod(request.method)
