1e0c46f59a10c4aedcfea1b09bd00980d6626a6a
Marco Vermeulen <vermeulen.mp@gmail.com>
2016-10-24 15:50:40 +0100

    Tweak vendors table definition.

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE sdkman_vendor_proxy;
 \c sdkman_vendor_proxy
-CREATE TABLE vendors(id varchar(50) primary key, token varchar(100), name varchar(50));
+CREATE TABLE vendors(id varchar(32) primary key, token varchar(64), name varchar(100));
