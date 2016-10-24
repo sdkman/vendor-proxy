fc0f44f83a20b866b58fee4d302019e93f3737b1
Marco Vermeulen <vermeulen.mp@gmail.com>
2016-10-24 11:32:26 +0100

    Inject wsclient for play 2.5 compliance.

--- app/controllers/ProxyController.scala
+++ app/controllers/ProxyController.scala
@@ -2,22 +2,21 @@ package controllers
 
 import com.google.inject.Inject
 import play.api.Logger
-import play.api.Play._
-import play.api.libs.ws.WS
+import play.api.libs.ws.WSClient
 import play.api.mvc.Controller
 import repos.ConsumerRepo
 import security.AsConsumer
-import utils.{VendorProxyConfig, RequestHeaders}
+import utils.{RequestHeaders, VendorProxyConfig}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class ProxyController @Inject()(val config: VendorProxyConfig, implicit val cr: ConsumerRepo)
+class ProxyController @Inject()(val config: VendorProxyConfig, wSClient: WSClient, implicit val cr: ConsumerRepo)
   extends Controller
     with RequestHeaders {
 
   def execute(service: String) = AsConsumer(parse.json) { (request, consumerName) =>
     Logger.info(s"Proxying $service on behalf of $consumerName")
-    WS.url(config.apiUrl(service))
+    wSClient.url(config.apiUrl(service))
       .withHeaders(tokenHeader(service), consumerHeader(consumerName))
       .withMethod(request.method)
       .withBody(request.body)
